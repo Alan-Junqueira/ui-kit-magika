5140a1742ec56c6bdec16bba3eba03dc0edf5341
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-25 11:26:27 -0300

    Footer responsive finalized

--- src/components/partials/Footer/styled.ts
+++ src/components/partials/Footer/styled.ts
@@ -12,6 +12,14 @@ export const FooterContent = styled.div`
   padding: 72px 120px;
   width: 100%;
 
+  @media (min-width: 1200px) {
+    display: grid;
+    grid-template-areas: 'header nav' 'footer footer';
+    padding-bottom: 72px;
+
+    justify-content: space-between;
+  }
+
   @media (max-width: 1199px) {
     padding: 72px 0;
   }
@@ -32,6 +40,11 @@ export const FooterContent = styled.div`
 
     color: ${(props) => props.theme.colors['white-1']};
 
+    @media (min-width: 1200px) {
+      grid-area: footer;
+      margin-inline: 72px;
+    }
+
     > div {
       position: absolute;
       content: '';
@@ -46,18 +59,58 @@ export const FooterContent = styled.div`
 `;
 
 export const FooterHeader = styled.header`
-  padding-inline: 24px;
-  margin-bottom: 64px;
+  @media (min-width: 1200px) {
+    grid-area: header;
+
+    max-width: 388px;
+    width: 100%;
+  }
 
   @media (max-width: 1199px) {
-    padding-inline: 72px;
+    padding-inline: 120px;
     width: 49%;
     margin-bottom: 56px;
+  }
+
+  @media (max-width: 1100px) {
+    width: 55%;
+  }
+
+  @media (max-width: 982px) {
+    width: 60%;
+  }
+
+  @media (max-width: 898px) {
+    width: 65%;
+  }
+
+  @media (max-width: 830px) {
+    width: 70%;
+  }
+
+  @media (max-width: 764px) {
+    width: 75%;
+  }
+
+  @media (max-width: 720px) {
+    width: 80%;
+  }
+
+  @media (max-width: 674px) {
+    width: 90%;
+  }
+
+  @media (max-width: 594px) {
+    width: 100%;
+  }
+
+  @media (max-width: 425px) {
+    padding-inline: 24px;
+    margin-bottom: 64px;
   }
   > img.logo {
     display: inline-block;
 
-    width: 123px;
     height: auto;
 
     margin-bottom: 16px;
@@ -65,12 +118,14 @@ export const FooterHeader = styled.header`
     @media (max-width: 1199px) {
       width: 200px;
     }
+
+    @media (max-width: 425px) {
+      width: 123px;
+    }
   }
 
   > p {
     font-weight: 400;
-    font-size: ${(props) => props.theme.sizes['16px']};
-    line-height: 26px;
 
     margin-bottom: 32px;
 
@@ -80,12 +135,21 @@ export const FooterHeader = styled.header`
       font-size: ${(props) => props.theme.sizes['18px']};
       line-height: 28px;
     }
+
+    @media (max-width: 425px) {
+      font-size: ${(props) => props.theme.sizes['16px']};
+      line-height: 26px;
+    }
   }
 
   > div.social {
     display: flex;
     align-items: center;
     gap: 32px;
+
+    @media (min-width: 425px) and (max-width: 594px) {
+      gap: 10px;
+    }
 
     a {
       display: flex;
@@ -129,16 +193,38 @@ export const FooterHeader = styled.header`
 
 export const FooterNavigation = styled.nav`
   display: flex;
-  flex-direction: column;
-  gap: 64px;
 
-  padding-inline: 24px;
   margin-bottom: 40px;
+
+  @media (min-width: 1200px) {
+    grid-area: nav;
+    gap: 80px;
+  }
+
+  @media (max-width: 1199px) {
+    flex-direction: row;
+    gap: 64px;
+    padding-inline: 120px;
+  }
+
+  @media (max-width: 848px) {
+    flex-direction: column;
+    gap: 64px;
+  }
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    gap: 64px;
+    padding-inline: 24px;
+  }
 
   ul {
     display: flex;
     flex-direction: column;
     gap: 16px;
+
+    max-width: 184px;
+    width: 100%;
 
     span {
       font-weight: 500;
