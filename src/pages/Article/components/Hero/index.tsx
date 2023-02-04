a8c510cc699b49e11a2937016400d9af62c08103
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-02-04 16:22:18 -0300

    style: start to create Hero on page Article, style and header responsivity already done in w425, w1200, w1600

--- src/pages/Article/components/Hero/index.tsx
+++ src/pages/Article/components/Hero/index.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { ArticleHeroContainer, ArticleHeroContent } from './styled'
+import React from 'react';
+import { ArticleHeaderContent, ArticleHeroContainer, ArticleHeroContent } from './styled';
+
+import semicolonImage from '/assets/images/article-semicolon.svg';
+import article1 from '/assets/images/article-1.png';
+import { Button } from '../../../../components/Button';
 
 interface IHero extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const ArticleHero = ({...props}: IHero) => {
+export const ArticleHero = ({ ...props }: IHero) => {
   return (
     <ArticleHeroContainer {...props}>
       <ArticleHeroContent>
+        <ArticleHeaderContent>
+          <div className="header-texts">
+            <h1>Blog Article</h1>
+            <p>
+              Here are the reasons why objects can be the most popular school
+              today
+            </p>
+          </div>
+          <div className="image">
+            <img src={semicolonImage} alt="" />
+          </div>
+        </ArticleHeaderContent>
+        <main>
+          <div className="image">
+            <img src={article1} alt="" />
+          </div>
+          <div className="main-content">
+            <h2>Tips belajar menjadi menyenangkan</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
+              facilisis mauris sit amet massa.
+            </p>
+            <Button label="Read more" variant="big" />
+          </div>
+        </main>
         <h1>Teste</h1>
         <p>teste</p>
       </ArticleHeroContent>
     </ArticleHeroContainer>
-  )
-}
+  );
+};
--- src/pages/Article/components/Hero/styled.ts
+++ src/pages/Article/components/Hero/styled.ts
@@ -1,11 +1,128 @@
 import styled from 'styled-components';
 import { HeroContainer } from '../../../../components/Hero/styled';
 
-export const ArticleHeroContainer = styled(HeroContainer)`
-  
-`;
+export const ArticleHeroContainer = styled(HeroContainer)``;
 
 export const ArticleHeroContent = styled.div`
   max-width: 1600px;
-  width: 100%
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 425px) {
+    padding: 32px 26px;
+    gap: 32px;
+  }
+
+  @media (min-width: 426px) and (max-width: 1199px) {
+    padding: 45px 72px;
+    gap: 56px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 72px 152px;
+    gap: 72px;
+  }
+`;
+
+export const ArticleHeaderContent = styled.header`
+  display: grid;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 425px) {
+    grid-template-areas: 'image' 'texts';
+    gap: 26px;
+  }
+
+  @media (min-width: 426px) and (max-width: 1199px) {
+    grid-template-areas: 'texts image';
+    grid-template-columns: 1fr 190px;
+    gap: 88px;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-areas: 'texts image';
+    grid-template-columns: 1fr 410px;
+  }
+
+  div.header-texts {
+    grid-area: texts;
+
+    display: flex;
+    flex-direction: column;
+
+    @media (max-width: 425px) {
+      gap: 16px;
+    }
+
+    @media (min-width: 426px) and (max-width: 1199px) {
+      gap: 32px;
+    }
+
+    @media (min-width: 1200px) {
+      gap: 24px;
+    }
+
+    h1 {
+      color: ${(props) => props.theme.colors['white-1']};
+      font-weight: 700;
+
+      @media (max-width: 425px) {
+        font-size: ${(props) => props.theme.sizes['4xl']};
+        line-height: 46px;
+        text-align: center;
+      }
+
+      @media (min-width: 426px) and (max-width: 1199px) {
+        font-size: 5.8rem;
+        line-height: 72px;
+        font-weight: 500;
+      }
+
+      @media (min-width: 1200px) {
+        font-size: 10rem;
+        line-height: 100px;
+        letter-spacing: -1px;
+      }
+    }
+
+    p {
+      color: ${(props) => props.theme.colors['white-1']};
+      font-weight: 400;
+      opacity: 0.7;
+
+      @media (max-width: 425px) {
+        font-size: ${(props) => props.theme.sizes.base};
+        line-height: 26px;
+        text-align: center;
+      }
+
+      @media (min-width: 426px) and (max-width: 1199px) {
+        font-size: ${(props) => props.theme.sizes.sm};
+        line-height: 24px;
+      }
+
+      @media (min-width: 1200px) {
+        ont-size: 2.1rem;
+        line-height: 34px;
+
+        opacity: 0.8;
+      }
+    }
+  }
+
+  div.image {
+    grid-area: image;
+
+    @media(max-width: 425px){
+      padding-inline: 68px;
+    }
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
 `;
